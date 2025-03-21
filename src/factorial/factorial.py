2806213b8ad78405ab164b0f90569c35f1f6d010
Facundo <parodifacundo66@gmail.com>
2025-03-21 17:47:26 -0300

    modificaciones finales

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -9,36 +9,47 @@
 
 import sys
 
-
 def factorial(num): 
-    if num < 0: 
-        print("Factorial de un número negativo no existe")
+    if num < 0:
+        print("Factorial de un numero negativo no existe")
         return 0
-    elif num == 0: 
+    elif num == 0:
         return 1
-        
-    else: 
+    else:
         fact = 1
-        while(num > 1): 
-            fact *= num 
+        while(num > 1):
+            fact *= num
             num -= 1
         return fact 
 
-
+def calcular_factoriales_en_rango(desde, hasta):
+    factoriales = {}
+    for i in range(desde, hasta + 1):
+        factoriales[i] = factorial(i)
+    return factoriales
 
 if len(sys.argv) < 2:
-    num = input("Debe proporcionar un número para calcular su factorial: ")
-    while not num.isdigit():
-        num = input("Entrada no válida. Ingrese un número entero: ")
-    num = int(num)
+    num = input("Debe proporcionar un numero para calcular su factorial: ")
+    while not num.replace("-", "").isdigit():
+        num = input("Entrada no valida, ingrese un numero entero: ")
+    num = num.strip()
+    if '-' in num:
+        if num.startswith('-'):
+            num = num[1:]
+            for i in range(1, int(num)+1):
+                print(f"Factorial de {i}: {factorial(i)}")
+        else:
+            limites = num.split("-")
+            desde = int(limites[0])
+            hasta = int(limites[1])
+            print(calcular_factoriales_en_rango(desde, hasta))
+    else:
+        num = int(num)
+        print(f"el valor del factorial de {num} es {factorial(num)}")
 else:
     num = int(sys.argv[1])
-
-
-print("el valor del factorial de",num, "es",factorial(num))
-    
-   
-    
+    print(f"el valor del factorial de {num} es {factorial(num)}")
 
 
 
+# Comentario de prueba
